ba89c87f0ad58cf4c6d6ecc6c09da58cc4ec65de
nacseo <116015583+nacseo@users.noreply.github.com>
2024-01-25 18:51:56 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.04 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        int answer = 0;
+        String[] temp = new String[my_string.length()];
+        for(int i=0; i<temp.length; i++) {
+            temp[i] = my_string.substring(i);
+        }
+        for(int j=0; j<temp.length; j++) {
+            if(temp[j].equals(is_suffix)) answer = 1;
+        }
+        return answer;
+    }
+}
