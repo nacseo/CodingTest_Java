9ac76e27af4ef5b3c0bc5a96929c5eaeb2a2185f
nacseo <116015583+nacseo@users.noreply.github.com>
2024-02-22 18:10:25 +0900

    [level 0] Title: ad 제거하기, Time: 1.75 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        List<String> list = new ArrayList<>();
+        for(int i=0; i<strArr.length; i++) {
+            if(!strArr[i].contains("ad")) {
+                list.add(strArr[i]);
+            }
+        }
+        String[] answer = new String[list.size()];
+        for(int j=0; j<list.size(); j++) {
+            answer[j] = list.get(j);
+        }
+        return answer;
+    }
+}
