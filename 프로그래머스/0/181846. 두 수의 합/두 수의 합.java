8d339dd18c8dd3564d8690f5ce7ce09ab806d6c8
nacseo <116015583+nacseo@users.noreply.github.com>
2024-03-18 19:47:25 +0900

    [level 0] Title: 두 수의 합, Time: 236.17 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+
+class Solution {
+    public String solution(String a, String b) {
+        BigInteger x = new BigInteger(a);
+        BigInteger y = new BigInteger(b);
+        BigInteger sum = x.add(y);
+        return sum.toString();
+    }
+}
